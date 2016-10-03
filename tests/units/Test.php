e2bff46b2f0844a6285343754b6a6e57897fa2ad
jlttt <jlttt@laposte.net>
2016-10-04 00:38:06 +0200

    Update Test.php

--- tests/units/Test.php
+++ tests/units/Test.php
@@ -3,12 +3,12 @@ namespace jlttt\tests\units;
 use mageekguy\atoum;
 var_dump(file_exists(__DIR__ . '/../../src/Test.php'));
 require_once(dirname(dirname(__DIR__)) . '/src/Test.php');
-
+/*
 class Test extends atoum
 {
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
 }
-
+*/
 ?>
