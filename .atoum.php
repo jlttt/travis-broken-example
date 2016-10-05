5723dd814cde293d13f3ed831f1b534e0b409299
jean-luc <jean-luc@pmpconcept.com>
2016-10-05 22:28:02 +0200

    ignore build directory

--- .atoum.php
+++ .atoum.php
@@ -3,7 +3,7 @@ use mageekguy\atoum;
 
 $script->addDefaultReport();
 
-$cloverWriter = new atoum\report\writers\file('./build/logs/clover.xml');
+$cloverWriter = new atoum\writers\file('./build/logs/clover.xml');
 $cloverReport = new atoum\reports\asynchronous\clover();
 $cloverReport->addWriter($cloverWriter);
 
